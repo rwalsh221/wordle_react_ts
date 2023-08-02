98cfc1ab88fef06118f2beffd24e621d32b513ba
Richard Walsh <richwalsh2@googlemail.com>
2023-08-02 18:24:21 +0100

    style header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,14 @@ import classes from './Header.module.css';
 const Header = () => {
   return (
     <header className={classes.header}>
-      <h1>wordle</h1>
+      <h1 className={classes.header_container}>
+        <span className={classes.header_red}>w</span>
+        <span className={classes.header_red}>o</span>
+        <span className={classes.header_blue}>r</span>
+        <span className={classes.header_blue}>d</span>
+        <span className={classes.header_green}>l</span>
+        <span className={classes.header_green}>e</span>
+      </h1>
     </header>
   );
 };
